b09f2a0a73af2a3a6858d598bfd4ded0d3edcf26
PrintGjb <742922612@qq.com>
2020-07-06 23:30:01 +0800

    Site updated: 2020-07-06 23:30:00

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var minifycss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var htmlclean = require('gulp-htmlclean');
+var imagemin = require('gulp-imagemin');
+
+// 压缩html
+gulp.task('minify-html', function() {
+    return gulp.src('./public/**/*.html')
+        .pipe(htmlclean())
+        .pipe(htmlmin({
+            removeComments: true,	//清除HTML注释
+            minifyJS: true,			//压缩页面JS
+            minifyCSS: true,		//压缩页面CSS
+            minifyURLs: true,
+        }))
+        .pipe(gulp.dest('./public'))
+});
+// 压缩css
+gulp.task('minify-css', function() {
+    return gulp.src('./public/**/*.css')
+        .pipe(minifycss({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest('./public'));
+});
+// 压缩js
+gulp.task('minify-js', function() {
+    return gulp.src('./public/js/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./public'));
+});
+// 压缩图片
+gulp.task('minify-images', function() {
+    return gulp.src('./public/images/**/*.*')
+        .pipe(imagemin(
+        [imagemin.gifsicle({'optimizationLevel': 3}),
+        imagemin.jpegtran({'progressive': true}),
+        imagemin.optipng({'optimizationLevel': 7}),
+        imagemin.svgo()],
+        {'verbose': true}))
+        .pipe(gulp.dest('./public/images'))
+});
+
+gulp.task('minify-img', function() {
+    return gulp.src('./public/img/**/*.*')  // 1. 找到图片
+        .pipe(imagemin(						// 2. 压缩图片
+        [imagemin.gifsicle({'optimizationLevel': 3}),
+        imagemin.jpegtran({'progressive': true}),
+        imagemin.optipng({'optimizationLevel': 7}),
+        imagemin.svgo()],
+        {'verbose': true}))
+        .pipe(gulp.dest('./public/img'))	// 3. 另存图片
+});
+
+// 执行 gulp 命令时执行的任务
+gulp.task('default', gulp.parallel(
+    'minify-html','minify-css','minify-js','minify-images', 'minify-img'
+));
